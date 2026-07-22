369ba5eed6e193738681e98e94a13d67108127f6
slemo54 <slemook00@gmail.com>
2026-07-22 17:15:34 +0200

    Add Clockify V2 data foundation

--- lib/feature-flags.ts
+++ lib/feature-flags.ts
@@ -2,3 +2,6 @@ export function isClockifyEnabled(): boolean {
   return process.env.NEXT_PUBLIC_CLOCKIFY_ENABLED === "true";
 }
 
+export function isClockifyV2Enabled(): boolean {
+  return process.env.NEXT_PUBLIC_CLOCKIFY_V2_ENABLED === "true";
+}
